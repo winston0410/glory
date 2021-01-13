3bf8474be7fa7ab1efef129f84ea9536684e8878
hugo <>
2021-01-13 18:01:31 +0800

    Extract prefixer function

--- addon/helper.js
+++ addon/helper.js
@@ -1,4 +1,15 @@
+import { prefix as prefixAll } from 'inline-style-prefixer'
+import { camelCaseProperty } from 'css-in-js-utils'
+import { pipe, map } from 'rambda'
+
 const assembleClassName = (renderer, name) =>
 	renderer.pfx + (name || renderer.hash())
 
-export { assembleClassName }
+const addPrefix = pipe(
+	Object.entries,
+	map(([prop, value]) => [camelCaseProperty(prop), value]),
+	Object.fromEntries,
+	prefixAll
+)
+
+export { assembleClassName, addPrefix }
--- addon/prefixer.js
+++ addon/prefixer.js
@@ -1,23 +1,12 @@
 'use strict'
 
-import { prefix as prefixAll } from 'inline-style-prefixer'
-import { camelCaseProperty } from 'css-in-js-utils'
-import { pipe, map } from 'rambda'
+import { addPrefix } from './helper'
 
 const addOn = function (renderer) {
 	const put = renderer.put
 
 	renderer.put = function withPrefixer(selector, decls, atRule) {
-		const prefixedDecl = pipe(
-			Object.entries,
-			map(([prop, value]) => [camelCaseProperty(prop), value]),
-			Object.fromEntries,
-			prefixAll
-		)(decls)
-
-		console.log('check prefix result', prefixedDecl)
-
-		put(selector, prefixedDecl, atRule)
+		put(selector, addPrefix(decls), atRule)
 	}
 }
 
