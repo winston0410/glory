b3b0918501169fca09a1a7e632c2711d239098cd
hugo <>
2021-01-13 18:22:55 +0800

    Add test for drule

--- tests/drule.test.js
+++ tests/drule.test.js
@@ -9,7 +9,9 @@ import addOnDrule from '../addon/drule'
 function createNano(config) {
 	const nano = create(config)
 
-	addonRule(nano)
+	addOnCache(nano)
+	addOnRule(nano)
+	addOnDrule(nano)
 
 	return nano
 }
@@ -25,5 +27,32 @@ describe('drule()', function () {
 		addOnDrule(nano)
 
 		expect(typeof nano.drule).toBe('function')
+	})
+
+	it('puts CSS styles', function () {
+		const nano = createNano({
+			pfx: 'test-'
+		})
+
+		nano.put = jest.fn()
+
+		nano.drule(
+			{
+				color: 'red'
+			},
+			'foobar'
+		)
+
+		expect(nano.put).toHaveBeenCalledTimes(1)
+		expect(nano.put).toHaveBeenCalledWith('.test-foobar', { color: 'red' })
+		// expect(classNames).toBe('test-foobar')
+	})
+
+	it('changes dynamically by callback', function () {
+		const nano = createNano({
+			pfx: 'test-'
+		})
+
+		nano.put = jest.fn()
 	})
 })
