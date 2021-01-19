ad3aa3252a772b7fc775389980a1b8a662791e27
hugo <>
2021-01-19 15:14:13 +0800

    Add virtual in preset

--- src/preset.js
+++ src/preset.js
@@ -3,12 +3,14 @@ import _nesting from './addon/nesting'
 import _prefixer from './addon/prefixer'
 import _keyframes from './addon/keyframes'
 import _rule from './addon/rule'
+import _virtual from './addon/virtual'
 
 const nano = create()
 
 _nesting(nano)
 _rule(nano)
 _keyframes(nano)
+_virtual(nano)
 _prefixer(nano)
 
 const { rule, keyframes } = nano
