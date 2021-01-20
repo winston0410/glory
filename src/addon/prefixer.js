2f4918c3b19d2e8452ce4d1da3a2dcc2ab57769f
kghugo <howtostudycantonese@gmail.com>
2021-01-20 22:37:56 +0800

    Add prefixer support for virtual()

--- src/addon/prefixer.js
+++ src/addon/prefixer.js
@@ -3,17 +3,20 @@
 import { addPrefix } from '../helper'
 
 const addOn = function (renderer) {
-	const put = renderer.put
-
-	renderer.put = function withPrefixer(selector, decls, atRule) {
-		return put(selector, addPrefix(decls), atRule)
+	if (renderer.put) {
+		const put = renderer.put
+		renderer.put = (selector, decls, atRule) =>
+			put(selector, addPrefix(decls), atRule)
 	}
 
 	if (renderer.keyframes) {
 		const keyframes = renderer.keyframes
-		renderer.keyframes = (decls, name) => {
-			return keyframes(addPrefix(decls), name)
-		}
+		renderer.keyframes = (decls, name) => keyframes(addPrefix(decls), name)
+	}
+
+	if (renderer.virtual) {
+		const virtual = renderer.virtual
+		renderer.virtual = (decls) => virtual(addPrefix(decls))
 	}
 }
 
