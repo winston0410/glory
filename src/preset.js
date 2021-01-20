5b82ac032f12af6b04c2079a647731e29b4db162
kghugo <howtostudycantonese@gmail.com>
2021-01-20 09:52:21 +0800

    Update preset

--- src/preset.js
+++ src/preset.js
@@ -1,14 +1,10 @@
 import { create } from './index'
-import _nesting from './addon/nesting'
 import _prefixer from './addon/prefixer'
 import _keyframes from './addon/keyframes'
-import _rule from './addon/rule'
 import _virtual from './addon/virtual'
 
 const nano = create()
 
-_nesting(nano)
-_rule(nano)
 _keyframes(nano)
 _virtual(nano)
 _prefixer(nano)
