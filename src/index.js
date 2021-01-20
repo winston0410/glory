5271e087517e05cf5980e3b0e8662208fbd54a46
kghugo <howtostudycantonese@gmail.com>
2021-01-21 07:08:04 +0800

    Add polyfill packages for using worker in Node.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import { isBrowser } from 'browser-or-node'
 import joli from '@blackblock/joli-string'
 import greenlet from 'greenlet'
+import Worker from 'tiny-worker'
+import Blob from 'cross-blob'
 const isProduction = process.env.NODE_ENV !== 'production'
 
 const shouldAddSpace = (selector) =>
