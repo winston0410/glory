b748770023cd31da04977f55af330e9dd32d610d
kghugo <howtostudycantonese@gmail.com>
2021-01-23 10:38:13 +0800

    Only generate new style if renderer.ksh is undefined

--- src/addon/keyframes.js
+++ src/addon/keyframes.js
@@ -9,8 +9,12 @@ const addOn = function (renderer, config = {}) {
 	const prefixedKeyframes = prefixes.map((prefix) => `@${prefix}keyframes`)
 
 	if (renderer.client) {
-		// Craete @keyframe Stylesheet `ksh`.
-		document.head.appendChild((renderer.ksh = document.createElement('style')))
+		if (!renderer.ksh) {
+			// Craete @keyframe Stylesheet `ksh`.
+			document.head.appendChild(
+				(renderer.ksh = document.createElement('style'))
+			)
+		}
 	}
 
 	renderer.keyframes = function (decls, name) {
