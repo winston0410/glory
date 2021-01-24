fa55db3b7904ab4aeeefb49445fd72b653b44b96
kghugo <howtostudycantonese@gmail.com>
2021-01-24 22:50:36 +0800

    Refactor test to share same function call under a common describe()

--- tests/hydration.test.js
+++ tests/hydration.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 'use strict'
 
+//Is it an integration test or unit test? Is there anything wrong with this test code
 const env = require('./env')
 import { create } from '../src/index'
 import addOnHydration from '../src/addon/hydration'
@@ -31,210 +32,221 @@ function createKeyframesNano(config) {
 }
 
 describe('hydration', function () {
-	describe('when using put() or rule()', function () {
-		it('should prevent basic declaration found in stylesheet from re-rendering', function () {
-			const mockStylesheet = document.createElement('style')
-			mockStylesheet.textContent = '.one{display:block;}'
-			document.head.appendChild(mockStylesheet)
-
-			const nano = createRuleNano({
-				sh: mockStylesheet
-			})
-
-			const putMock = jest.spyOn(nano, 'put')
-
-			nano.put('.one', {
-				display: 'block'
-			})
-
-			expect(putMock).toHaveBeenCalledTimes(0)
-		})
-
-		it('should re-render basic declaration, if props in stylesheet and in Javascript are different', function () {
-			const mockStylesheet = document.createElement('style')
-			mockStylesheet.textContent = '.one{display:block;}'
-			document.head.appendChild(mockStylesheet)
-
-			const nano = createRuleNano({
-				sh: mockStylesheet
-			})
-
-			const putMock = jest.spyOn(nano, 'put')
-
-			nano.put('.one', {
-				display: 'block',
-				color: 'red'
-			})
-
-			expect(putMock).toHaveBeenCalledTimes(1)
-		})
+	it('should be installed without throwing error', function () {
+		const nano = create()
+		addOnHydration(nano)
+		// expect(nano).toBe('function')
 	})
 
-	describe('when using virtual()', function () {
-		it('should prevent basic declaration found in stylesheet from re-rendering', function () {
-			const mockStylesheet = document.createElement('style')
-			mockStylesheet.textContent = '.a{display:block;}'
-			document.head.appendChild(mockStylesheet)
+	describe('when server-side generated stylesheet is provided', function () {
+		describe('when using it with put() or rule()', function () {
+			it('should prevent basic declaration found in stylesheet from re-rendering', function () {
+				const mockStylesheet = document.createElement('style')
+				mockStylesheet.textContent = '.one{display:block;}'
+				document.head.appendChild(mockStylesheet)
 
-			const nano = createVirtualNano({
-				sh: mockStylesheet
-			})
+				const nano = createRuleNano({
+					sh: mockStylesheet
+				})
 
-			const atomicMock = jest.spyOn(nano, 'atomic')
+				const putMock = jest.spyOn(nano, 'put')
 
-			addOnHydration(nano)
-
-			nano.virtual({
-				display: 'block'
-			})
-
-			expect(atomicMock).toHaveBeenCalledTimes(0)
-		})
-
-		it('should re-render basic declaration, if props in stylesheet and in Javascript are different', function () {
-			const mockStylesheet = document.createElement('style')
-			mockStylesheet.textContent = '.a{display:block;}'
-			document.head.appendChild(mockStylesheet)
-
-			const nano = createVirtualNano({
-				sh: mockStylesheet
-			})
-
-			nano.putRaw = jest.fn()
-
-			const atomicMock = jest.spyOn(nano, 'atomic')
-
-			addOnHydration(nano)
-
-			nano.virtual({
-				display: 'block',
-				color: 'red'
-			})
-
-			if (env.isClient) {
-				expect(nano.putRaw).toHaveBeenCalledTimes(1)
-				const result = nano.putRaw.mock.calls[0][0]
-				expect(result).toBe('.b{color:red;}')
-			}
-
-			expect(atomicMock).toHaveBeenCalledTimes(1)
-		})
-
-		it('should prevent media-queries found in stylesheet from re-rendering', function () {
-			const mockStylesheet = document.createElement('style')
-			mockStylesheet.textContent = '@media screen{.a{display:block;}}'
-			document.head.appendChild(mockStylesheet)
-
-			const nano = createVirtualNano({
-				sh: mockStylesheet
-			})
-
-			const atomicMock = jest.spyOn(nano, 'atomic')
-
-			addOnHydration(nano)
-
-			nano.virtual({
-				'@media screen': {
+				nano.put('.one', {
 					display: 'block'
+				})
+
+				expect(putMock).toHaveBeenCalledTimes(0)
+			})
+
+			it('should re-render basic declaration, if props in stylesheet and in Javascript are different', function () {
+				const mockStylesheet = document.createElement('style')
+				mockStylesheet.textContent = '.one{display:block;}'
+				document.head.appendChild(mockStylesheet)
+
+				const nano = createRuleNano({
+					sh: mockStylesheet
+				})
+
+				const putMock = jest.spyOn(nano, 'put')
+
+				nano.put('.one', {
+					display: 'block',
+					color: 'red'
+				})
+
+				expect(putMock).toHaveBeenCalledTimes(1)
+			})
+		})
+
+		describe('when using it with virtual()', function () {
+			it('should prevent rules found in stylesheet from being inserted again.', function () {
+				const mockStylesheet = document.createElement('style')
+				mockStylesheet.textContent = '.a{display:block;}'
+				document.head.appendChild(mockStylesheet)
+
+				const nano = createVirtualNano({
+					sh: mockStylesheet
+				})
+
+				const atomicMock = jest.spyOn(nano, 'atomic')
+
+				addOnHydration(nano)
+
+				nano.virtual({
+					display: 'block'
+				})
+
+				expect(atomicMock).toHaveBeenCalledTimes(0)
+			})
+
+			it('should re-render basic declaration, if props in stylesheet and in Javascript are different', function () {
+				const mockStylesheet = document.createElement('style')
+				mockStylesheet.textContent = '.a{display:block;}'
+				document.head.appendChild(mockStylesheet)
+
+				const nano = createVirtualNano({
+					sh: mockStylesheet
+				})
+
+				nano.putRaw = jest.fn()
+
+				const atomicMock = jest.spyOn(nano, 'atomic')
+
+				addOnHydration(nano)
+
+				nano.virtual({
+					display: 'block',
+					color: 'red'
+				})
+
+				if (env.isClient) {
+					expect(nano.putRaw).toHaveBeenCalledTimes(1)
+					const result = nano.putRaw.mock.calls[0][0]
+					expect(result).toBe('.b{color:red;}')
 				}
+
+				expect(atomicMock).toHaveBeenCalledTimes(1)
 			})
 
-			expect(atomicMock).toHaveBeenCalledTimes(0)
+			it('should prevent media-queries found in stylesheet from re-rendering', function () {
+				const mockStylesheet = document.createElement('style')
+				mockStylesheet.textContent = '@media screen{.a{display:block;}}'
+				document.head.appendChild(mockStylesheet)
+
+				const nano = createVirtualNano({
+					sh: mockStylesheet
+				})
+
+				const atomicMock = jest.spyOn(nano, 'atomic')
+
+				addOnHydration(nano)
+
+				nano.virtual({
+					'@media screen': {
+						display: 'block'
+					}
+				})
+
+				expect(atomicMock).toHaveBeenCalledTimes(0)
+			})
+
+			it('should not generate wrong className if third party styling is inserted before generated styling', function () {
+				const mockStylesheet = document.createElement('style')
+				mockStylesheet.textContent = 'body{display:block;}.a{display:block;}'
+				document.head.appendChild(mockStylesheet)
+
+				const nano = createVirtualNano({
+					sh: mockStylesheet
+				})
+
+				const atomicMock = jest.spyOn(nano, 'atomic')
+
+				addOnHydration(nano)
+
+				const className = nano.virtual({
+					display: 'block'
+				})
+
+				expect(className).toBe('.a')
+				expect(atomicMock).toHaveBeenCalledTimes(0)
+			})
+
+			it('should not generate wrong className if media queries with same declaration is inserted after generated styling', function () {
+				const mockStylesheet = document.createElement('style')
+				mockStylesheet.textContent =
+					'body{display:block;}.a{display:block;}@media screen{.b{display:block;}}'
+				document.head.appendChild(mockStylesheet)
+
+				const nano = createVirtualNano({
+					sh: mockStylesheet
+				})
+
+				const atomicMock = jest.spyOn(nano, 'atomic')
+
+				addOnHydration(nano)
+
+				const className = nano.virtual({
+					display: 'block'
+				})
+
+				expect(className).toBe('.a')
+				expect(atomicMock).toHaveBeenCalledTimes(0)
+			})
 		})
 
-		it('should not generate wrong className if third party styling is inserted before generated styling', function () {
-			const mockStylesheet = document.createElement('style')
-			mockStylesheet.textContent = 'body{display:block;}.a{display:block;}'
-			document.head.appendChild(mockStylesheet)
+		// it('should prevent media-queries found in stylesheet from re-rendering', function() {
+		// 	const mockStylesheet = document.createElement('style')
+		// 	mockStylesheet.textContent = '@media screen{.one{display:block;}}'
+		// 	document.head.appendChild(mockStylesheet)
+		//
+		// 	const nano = create({
+		// 		sh: mockStylesheet
+		// 	})
+		//
+		// 	const putMock = jest.spyOn(nano, 'put')
+		//
+		// 	addOnHydration(nano)
+		//
+		// 	nano.put('.one', {
+		// 		'@media screen': {
+		// 			display: 'block'
+		// 		}
+		// 	})
+		//
+		// 	expect(putMock).toHaveBeenCalledTimes(0)
+		// })
 
-			const nano = createVirtualNano({
-				sh: mockStylesheet
+		describe('when using keyframes()', function () {
+			it('should prevent keyframes found in stylesheet from re-rendering', function () {
+				const mockStylesheet = document.createElement('style')
+				mockStylesheet.textContent =
+					'@keyframes a{to{transform:rotate(360deg);}}'
+				document.head.appendChild(mockStylesheet)
+
+				const nano = create({
+					sh: mockStylesheet
+				})
+
+				addonKeyframes(nano, {
+					prefixes: ['']
+				})
+				addOnHydration(nano)
+
+				const putRawMock = jest.spyOn(nano, 'putRaw')
+
+				const frameName = nano.keyframes({
+					to: {
+						transform: 'rotate(360deg)'
+					}
+				})
+
+				const nanoForComparison = create()
+
+				expect(frameName).toBe(nanoForComparison.hash())
+				expect(putRawMock).toHaveBeenCalledTimes(0)
 			})
-
-			const atomicMock = jest.spyOn(nano, 'atomic')
-
-			addOnHydration(nano)
-
-			const className = nano.virtual({
-				display: 'block'
-			})
-
-			expect(className).toBe('.a')
-			expect(atomicMock).toHaveBeenCalledTimes(0)
-		})
-
-		it('should not generate wrong className if media queries with same declaration is inserted after generated styling', function () {
-			const mockStylesheet = document.createElement('style')
-			mockStylesheet.textContent =
-				'body{display:block;}.a{display:block;}@media screen{.b{display:block;}}'
-			document.head.appendChild(mockStylesheet)
-
-			const nano = createVirtualNano({
-				sh: mockStylesheet
-			})
-
-			const atomicMock = jest.spyOn(nano, 'atomic')
-
-			addOnHydration(nano)
-
-			const className = nano.virtual({
-				display: 'block'
-			})
-
-			expect(className).toBe('.a')
-			expect(atomicMock).toHaveBeenCalledTimes(0)
 		})
 	})
 
-	// it('should prevent media-queries found in stylesheet from re-rendering', function() {
-	// 	const mockStylesheet = document.createElement('style')
-	// 	mockStylesheet.textContent = '@media screen{.one{display:block;}}'
-	// 	document.head.appendChild(mockStylesheet)
-	//
-	// 	const nano = create({
-	// 		sh: mockStylesheet
-	// 	})
-	//
-	// 	const putMock = jest.spyOn(nano, 'put')
-	//
-	// 	addOnHydration(nano)
-	//
-	// 	nano.put('.one', {
-	// 		'@media screen': {
-	// 			display: 'block'
-	// 		}
-	// 	})
-	//
-	// 	expect(putMock).toHaveBeenCalledTimes(0)
-	// })
-
-	describe('when using keyframes()', function () {
-		it('should prevent keyframes found in stylesheet from re-rendering', function () {
-			const mockStylesheet = document.createElement('style')
-			mockStylesheet.textContent = '@keyframes a{to{transform:rotate(360deg);}}'
-			document.head.appendChild(mockStylesheet)
-
-			const nano = create({
-				sh: mockStylesheet
-			})
-
-			addonKeyframes(nano, {
-				prefixes: ['']
-			})
-			addOnHydration(nano)
-
-			const putRawMock = jest.spyOn(nano, 'putRaw')
-
-			const frameName = nano.keyframes({
-				to: {
-					transform: 'rotate(360deg)'
-				}
-			})
-
-			const nanoForComparison = create()
-
-			expect(frameName).toBe(nanoForComparison.hash())
-			expect(putRawMock).toHaveBeenCalledTimes(0)
-		})
-	})
+	describe('when server-side generated stylesheet is not provided', function () {})
 })
--- tests/keyframes.test.js
+++ tests/keyframes.test.js
@@ -11,62 +11,41 @@ function createNano(config) {
 	return nano
 }
 
-describe('installing keyframes()', function () {
+describe('keyframes()', function () {
 	it('should become the method of the renderer', function () {
 		const nano = createNano()
 		expect(typeof nano.keyframes).toBe('function')
 	})
 
-	describe('when calling keyframes()', function () {
-		it('should return the name of the keyframe', function () {
-			const nano = createNano()
-			const name = nano.keyframes({
-				to: {
-					transform: 'rotate(360deg)'
-				}
-			})
-
-			const nanoForCoparison = createNano()
-
-			expect(typeof name).toBe('string')
-			expect(name).toBe(nanoForCoparison.hash())
-			expect(name.length > 0).toBe(true)
-		})
-
-		it('should insert the keyframe into the stylesheet', function () {
-			const nano = create()
-
-			addonKeyframes(nano, {
-				prefixes: ['']
-			})
-
-			const putRawMock = jest.spyOn(nano, 'putRaw')
-
-			const name = nano.keyframes({
-				to: {
-					transform: 'rotate(360deg)'
-				}
-			})
-
-			if (env.isServer) {
-				expect(nano.raw.includes('to{transform:rotate(360deg);}')).toBe(true)
-			}
-
-			expect(putRawMock).toHaveBeenCalledTimes(1)
-		})
-
+	describe('when calling keyframes() with declarations', function () {
 		describe('when prefixes is not provided', function () {
+			const nano = createNano()
+			const putRawMock = jest.spyOn(nano, 'putRaw')
+			const name = nano.keyframes({
+				to: {
+					transform: 'rotate(360deg)'
+				}
+			})
+
+			it('should return the name of the keyframe', function () {
+				expect(typeof name).toBe('string')
+				expect(name.length > 0).toBe(true)
+			})
+
+			it('should return a name based on the hash function', function () {
+				const nanoForCoparison = createNano()
+				expect(name).toBe(nanoForCoparison.hash())
+			})
+
+			it('should insert the keyframe into the stylesheet', function () {
+				if (env.isServer) {
+					expect(nano.raw.includes('to{transform:rotate(360deg);}')).toBe(true)
+				}
+
+				expect(putRawMock).toHaveBeenCalledTimes(1)
+			})
+
 			it('should insert the keyframe to stylesheet with all prefixes', function () {
-				const nano = create()
-
-				addonKeyframes(nano)
-
-				const name = nano.keyframes({
-					to: {
-						transform: 'rotate(360deg)'
-					}
-				})
-
 				if (env.isServer) {
 					const result = nano.raw
 					expect(result.includes('@-webkit-keyframes')).toBe(true)
@@ -75,10 +54,22 @@ describe('installing keyframes()', function () {
 					expect(result.includes('@keyframes')).toBe(true)
 				}
 			})
+
+			describe('when keyframes() is called with duplicated declarations', function () {
+				it('should not insert identical keyframe into the stylesheet', function () {
+					nano.keyframes({
+						to: {
+							transform: 'rotate(360deg)'
+						}
+					})
+
+					expect(nano.putRaw).toHaveBeenCalledTimes(1)
+				})
+			})
 		})
 
 		describe('when prefixes is provided', function () {
-			it('should insert the keyframe to stylesheet with the provided prefixes', function () {
+			it('should insert the keyframe to stylesheet with the provided prefixes only', function () {
 				const nano = create()
 
 				addonKeyframes(nano, {
@@ -100,29 +91,19 @@ describe('installing keyframes()', function () {
 				}
 			})
 		})
+	})
 
-		describe('when keyframes() is called with duplicated declarations', function () {
-			it('should not insert identical keyframe into the stylesheet', function () {
-				const nano = create()
+	describe('when calling keyframes() without any argument', function () {
+		const nano = createNano()
+		const putRawMock = jest.spyOn(nano, 'putRaw')
+		const name = nano.keyframes()
 
-				addonKeyframes(nano)
+		it('should not return anything', function () {
+			// console.log('check name', name)
+		})
 
-				const putRawMock = jest.spyOn(nano, 'putRaw')
-
-				nano.keyframes({
-					to: {
-						transform: 'rotate(360deg)'
-					}
-				})
-
-				nano.keyframes({
-					to: {
-						transform: 'rotate(360deg)'
-					}
-				})
-
-				expect(nano.putRaw).toHaveBeenCalledTimes(1)
-			})
+		it('should not insert anything into the stylesheet', function () {
+			expect(nano.putRaw).toHaveBeenCalledTimes(0)
 		})
 	})
 })
