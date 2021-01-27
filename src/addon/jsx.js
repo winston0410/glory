89d6f0bfc882b06f44818296f8b208b73450f264
Jacky Yu <jacky.yu@perfectshape.com.hk>
2021-01-27 15:26:33 +0800

    Refactor for addon/jsx

--- src/addon/jsx.js
+++ src/addon/jsx.js
@@ -1,29 +1,28 @@
 'use strict'
-import { isProduction } from '../helper'
 import safeIsObj from 'safe-is-obj'
+
+const createComponent = (Tag, callback, renderer) =>
+	function Component(props = {}) {
+		if (!Tag) return null
+		if (callback) {
+			const stylingObject = callback(props)
+
+			if (safeIsObj(stylingObject)) {
+				props.className = renderer.virtual(stylingObject)
+			}
+		}
+
+		return renderer.h(props.as || Tag, props)
+	}
 
 const addOn = function(renderer) {
 	renderer.jsx = (Tag, callback) => {
-		if (!renderer.h) {
+		if (!renderer.h)
 			throw new Error(
 				'You need to set jsx factory function as renderer.h before using renderer.jsx.'
 			)
-		}
 
-		function Component(props = {}) {
-			if (!Tag) return null
-			if (callback) {
-				const stylingObject = callback(props)
-
-				if (safeIsObj(stylingObject)) {
-					props.className = renderer.virtual(stylingObject)
-				}
-			}
-
-			return renderer.h(props.as || Tag, props)
-		}
-
-		return Component
+		return createComponent(Tag, callback, renderer)
 	}
 }
 
