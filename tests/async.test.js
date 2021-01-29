9cd5be4225799710523669bf101b23788d3a62f8
hugo <>
2021-01-29 16:02:19 +0800

    Test async() add-on for client-side and server-side

--- tests/async.test.js
+++ tests/async.test.js
@@ -8,7 +8,7 @@ import addOnKeyframes from '../src/addon/keyframes'
 import addOnVirtual from '../src/addon/virtual'
 
 describe('async()', function() {
-	it('should turn functions from sync to async', function() {
+	it('should turn functions from sync to async', async function() {
 		const nano = create()
 		addOnVirtual(nano)
 		addOnKeyframes(nano)
@@ -20,5 +20,45 @@ describe('async()', function() {
 
 		expect(nano.keyframes.constructor.name).toBe('AsyncFunction')
 		expect(nano.virtual.constructor.name).toBe('AsyncFunction')
+	})
+
+	describe('when using an async keyframes()', function() {
+		it('should insert the keyframe into the stylesheet', async function() {
+			const nano = create()
+			addOnKeyframes(nano)
+			addOnAsync(nano)
+
+			const putRawMock = jest.spyOn(nano, 'putRaw')
+			const name = await nano.keyframes({
+				to: {
+					transform: 'rotate(360deg)'
+				}
+			})
+
+			if (env.isServer) {
+				expect(nano.raw.includes('to{transform:rotate(360deg);}')).toBe(true)
+			}
+
+			expect(putRawMock).toHaveBeenCalledTimes(1)
+		})
+	})
+
+	describe('when using an async virtual()', function() {
+		it('should inject CSS into stylesheet', async function() {
+			const nano = create()
+			addOnVirtual(nano)
+			addOnAsync(nano)
+
+			const putRawMock = jest.spyOn(nano, 'putRaw')
+
+			const className = await nano.virtual({
+				color: 'red'
+			})
+
+			if (env.isServer) {
+				expect(nano.raw).toBe('.a{color:red;}')
+			}
+			expect(putRawMock).toHaveBeenCalledTimes(1)
+		})
 	})
 })
