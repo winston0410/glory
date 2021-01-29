afe368b21e158d89f480abc20f6b636f89bbe2ab
hugo <>
2021-01-29 12:07:27 +0800

    Rename parameter name for assembleKeyframe()

--- src/helper.ts
+++ src/helper.ts
@@ -4,54 +4,54 @@ import safeIsObj from 'safe-is-obj'
 import { Renderer } from './type'
 
 const assembleClassName = (renderer: Renderer, name = '') =>
-	renderer.pfx + (name || renderer.hash())
+  renderer.pfx + (name || renderer.hash())
 
 const assembleDecl = (prop: string, value: string): string =>
-	`${hyphenateProperty(prop)}:${value};`
+  `${hyphenateProperty(prop)}:${value};`
 
 const assembleRule = (name: string, rule: string) => `${name}{${rule}}`
 
-const assembleKeyframe = (list: object): string => {
-	let result = ''
-	for (const key in list) {
-		result +=
-			typeof list[key] === 'string'
-				? `${key}:${list[key]};`
-				: `${key}{${assembleKeyframe(list[key])}}`
-	}
-	return result
+const assembleKeyframe = (decls: object): string => {
+  let result = ''
+  for (const prop in decls) {
+    result +=
+      typeof decls[prop] === 'string'
+        ? `${prop}:${decls[prop]};`
+        : `${prop}{${assembleKeyframe(decls[prop])}}`
+  }
+  return result
 }
 
 const isAtRule = (selector: string) =>
-	selector[0] === '@' && selector !== '@font-face'
+  selector[0] === '@' && selector !== '@font-face'
 
 const cssifyArray = (prop: string, value: string[]): string => {
-	let concatedDecl = ''
-	for (const currentValue of value) {
-		concatedDecl += assembleDecl(prop, currentValue)
-	}
-	return concatedDecl
+  let concatedDecl = ''
+  for (const currentValue of value) {
+    concatedDecl += assembleDecl(prop, currentValue)
+  }
+  return concatedDecl
 }
 
 const cssifyObject = (decls: object, callback?: Function) => {
-	let css = ''
-	for (const prop in decls) {
-		const value = decls[prop]
-		if (Array.isArray(value)) {
-			css += cssifyArray(prop, value)
-		} else if (safeIsObj(value) && callback) {
-			callback(prop, value)
-		} else {
-			css += assembleDecl(prop, value)
-		}
-	}
-	return css
+  let css = ''
+  for (const prop in decls) {
+    const value = decls[prop]
+    if (Array.isArray(value)) {
+      css += cssifyArray(prop, value)
+    } else if (safeIsObj(value) && callback) {
+      callback(prop, value)
+    } else {
+      css += assembleDecl(prop, value)
+    }
+  }
+  return css
 }
 
 const createCache = (renderer: Renderer, name: string) => {
-	if (!renderer[name]) {
-		renderer[name] = {}
-	}
+  if (!renderer[name]) {
+    renderer[name] = {}
+  }
 }
 
 const isEmptyObj = (obj: object) => Object.keys(obj).length === 0
@@ -59,15 +59,15 @@ const isEmptyObj = (obj: object) => Object.keys(obj).length === 0
 const isProduction = process.env.NODE_ENV === 'production'
 
 export {
-	assembleClassName,
-	addPrefix,
-	isAtRule,
-	cssifyObject,
-	cssifyArray,
-	assembleRule,
-	assembleDecl,
-	assembleKeyframe,
-	createCache,
-	isEmptyObj,
-	isProduction
+  assembleClassName,
+  addPrefix,
+  isAtRule,
+  cssifyObject,
+  cssifyArray,
+  assembleRule,
+  assembleDecl,
+  assembleKeyframe,
+  createCache,
+  isEmptyObj,
+  isProduction
 }
