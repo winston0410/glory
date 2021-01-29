c1c80b99be5284ef9a8c0cb5e96f7906caa6ed92
Jacky Yu <jacky.yu@perfectshape.com.hk>
2021-01-29 11:06:42 +0800

    Update Renderer type to optional properties

--- src/type.ts
+++ src/type.ts
@@ -1,11 +1,11 @@
 interface Renderer {
-	raw: String
-	pfx: String
-	client: Boolean
-	hash: Function
-	hashChars: String
+	raw?: String
+	pfx?: String
+	client?: Boolean
+	hash?: Function
+	hashChars?: String
 	sh?: HTMLStyleElement
-	putRaw: (cssString: string) => void
+	putRaw?: (cssString: string) => void
 	[prop: string]: any
 }
 
